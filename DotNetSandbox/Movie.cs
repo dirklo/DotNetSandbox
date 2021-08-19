82d22d94c86944ba4ccdcd5071e833d3a314c079
Rick Moore <dirklo@gmail.com>
2021-08-19 15:55:48 -0500

    add movie class

---
+++ DotNetSandbox/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace DotNetSandbox
+{
+    public class Movie
+    {
+        public int EpisodeId {
+            get;
+            set;
+        }
+        public string Title {
+            get;
+            set;
+        }
+        public string Director {
+            get;
+            set;
+        }
+        public string ReleaseDate {
+            get;
+            set;
+        }
+    }
+}
